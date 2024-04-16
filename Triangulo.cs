8a75eed83741132f59b9654a509fe546a67d2d92
ViniciusQuirino <viniciusquirino13@hotmail.com>
2024-04-16 16:33:15 -0300

    instance

---
+++ Triangulo.cs
@@ -0,0 +1,11 @@
+namespace C_sharp_classes_attributes_methods_static_members
+{
+    public class Triangulo
+    {
+        public double A;
+        public double B;
+        public double C;
+
+         
+    }
+}
